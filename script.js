2e5ad09dc260d6840923a424959e18863555e6a6
Pulok <pulok3968@gmail.com>
2021-09-23 03:27:36 +0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const makeNavLinksSmooth = ( ) => {
+    const navLinks = document.querySelectorAll( '.nav-link' );
+  
+    for ( let n in navLinks ) {
+      if ( navLinks.hasOwnProperty( n ) ) {
+        navLinks[ n ].addEventListener( 'click', e => {
+          e.preventDefault( );
+          document.querySelector( navLinks[ n ].hash )
+            .scrollIntoView( {
+              behavior: "smooth"
+            } );
+        } );
+      }
+    }
+  }
+  
+  const spyScrolling = ( ) => {
+    const sections = document.querySelectorAll( '.hero-bg' );
+    const element = document.getElementById('scrollspy-body');
+
+    element.onscroll = ( ) => {
+      const scrollPos = element.scrollTop;
+  
+      for ( let s in sections )
+        if ( sections.hasOwnProperty( s ) && (sections[ s ].offsetTop - element.offsetTop - 70) <= scrollPos ) {
+          const id = sections[ s ].id;
+          document.querySelector( '.active' ).classList.remove( 'active' );
+          document.querySelector( `a[href*=${ id }]` ).parentNode.classList.add( 'active' );
+        }
+    } 
+  }
+  
+  makeNavLinksSmooth( );
+  spyScrolling( );
